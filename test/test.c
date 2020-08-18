2fe1d02ca8ba52ff792a4792f41ee7b12698389f
Felix Wang <58910820@qq.com>
2020-08-18 20:37:32 +0800

    try to run a short progrome in AC mode

--- test/test.c
+++ test/test.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+//#include <stdio.h>
 #define SIM_START() asm("csrs 0x800,zero")
 #define SIM_STOP() asm("csrs  0x801,zero")
 
 int main()
 {
-	printf("Hello, World!\n");
+//	printf("Hello, World!\n");
     int a,b,c;
     SIM_START();
     a=1;

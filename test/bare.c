b09f96793504de2b367c5ab487d239a45e984f00
Yanyan Shen <yanyan.shen@outlook.com>
2020-08-20 00:17:36 +1000

    Set up stack pointer and remove prolog and epilog

--- test/bare.c
+++ test/bare.c
@@ -1,9 +1,10 @@
 #define SS() asm volatile ("csrs 0x800, zero")
 #define SE() asm volatile ("csrs 0x801, zero")
 
-int main(void)
+__attribute__((naked))int main(void)
 {
 
+    asm volatile("auipc sp, %0"::"i"(0x1000));
     SS();
     int a = 0;
     int b = 0;

70997b38013f5b73c5ec23593943f5c811f6ba1d
Yanyan Shen <yanyan.shen@outlook.com>
2020-08-20 00:19:04 +1000

    Generate objdump file for bare from ELF

--- test/mk_bare.sh
+++ test/mk_bare.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 riscv64-unknown-elf-gcc -nostdinc -nostdlib --entry main bare.c -o bare
 riscv64-unknown-elf-objcopy -O binary bare bare.bin
-
+riscv64-unknown-elf-objdump -D bare > bare.s

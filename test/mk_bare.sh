918b54e7159a0b82a66558a3cdf09c268857ab65
Yanyan Shen <yanyan.shen@outlook.com>
2020-09-02 23:30:29 +1000

    Add an assembly file and update script

--- test/mk_bare.sh
+++ test/mk_bare.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 riscv64-unknown-elf-gcc -nostdinc -nostdlib --entry main bare.c -o bare
 riscv64-unknown-elf-objcopy -O binary bare bare.bin
+riscv64-unknown-elf-objdump -D bare > bare.s
 
+riscv64-unknown-elf-as -o asm asm.S

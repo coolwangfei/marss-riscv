8def7da85856235c4f7093ed118e1f268c24acbf
Felix Wang <58910820@qq.com>
2020-09-03 09:33:21 +0800

    add mk asm and merge yy's edition

--- test/mk_bare.sh
+++ test/mk_bare.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 riscv64-unknown-elf-gcc -nostdinc -nostdlib --entry main bare.c -o bare
 riscv64-unknown-elf-objcopy -O binary bare bare.bin
-<<<<<<< HEAD
 riscv64-unknown-elf-objdump -D bare > bare.asm
-=======
-riscv64-unknown-elf-objdump -D bare > bare.s
 
-riscv64-unknown-elf-as -o asm asm.S
->>>>>>> 918b54e7159a0b82a66558a3cdf09c268857ab65
